be846d4680f7ab2560898dfcaa1a2b207849e2f6
gomesmarcos <gomes.marcosjf@gmail.com>
2023-06-27 23:54:11 -0300

    fix links on components

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const Header = () => {
   return (
@@ -7,12 +8,21 @@ const Header = () => {
 
       <Navbar expand="lg" variant='dark' bg="dark" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">ProShop</Navbar.Brand>
+          <LinkContainer to='/'>
+            <Navbar.Brand>ProShop</Navbar.Brand>
+          </LinkContainer>
+
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-              <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i>&nbsp;Cart</Nav.Link>
-              <Nav.Link href="/login"><i className='fas fa-user'></i>&nbsp;Login</Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link><i className='fas fa-shopping-cart'></i>&nbsp;Cart</Nav.Link>
+              </LinkContainer>
+
+              <LinkContainer to="/login">
+                <Nav.Link><i className='fas fa-user'></i>&nbsp;Login</Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
