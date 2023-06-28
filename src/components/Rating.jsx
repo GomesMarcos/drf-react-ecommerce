dcb3ab39fb2973fb3a11f4834dde098c3ca6a87d
gomesmarcos <gomes.marcosjf@gmail.com>
2023-06-27 23:11:13 -0300

    WIP components, add some mocks

---
+++ src/components/Rating.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Rating = ({ value, text, color }) => {
+  const stars = []
+  for (let index = 0; index < 5; index++) {
+    stars.push(
+      <span key={index}>
+        <i style={{ color }} className={setClassNameByRatingValue(value, index)}></i>
+      </span>
+    )
+  }
+  return (
+    <div className='rating'>
+      {stars} {text}
+    </div>
+  );
+}
+
+function setClassNameByRatingValue(value, index) {
+  return value >= index + 1 ? 'fas fa-star' : value >= index + 0.5 ? 'fas fa-star-half-alt' : 'far fa-star'
+}
+
+export default Rating;
